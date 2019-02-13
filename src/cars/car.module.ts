2b934346c251676ed4e2bd6c92fac50737fccdab
Abdul Sami <sami-abdul@outlook.com>
2019-02-13 17:23:31 +0500

    Update car.module.ts

--- src/cars/car.module.ts
+++ src/cars/car.module.ts
@@ -3,9 +3,6 @@ import { CarController } from './car.controller';
 import { CarService } from './car.service';
 import { MiddlewareConsumer } from '@nestjs/common/interfaces/middleware';
 import { EnvConfig } from '../common/config/env';
-import { JwtauthenticationMiddleware } from '../common/middleware/jwtauthentication.middleware';
-import { HlfcredsgeneratorMiddleware } from '../common/middleware/hlfcredsgenerator.middleware';
-import { AuthenticationModule } from '../core/authentication/authentication.module';
 import { ChainModule } from '../core/chain/chain.module';
 
 @Module({
@@ -16,17 +13,9 @@ import { ChainModule } from '../core/chain/chain.module';
         CarService,
     ],
     imports: [
-        AuthenticationModule,
         ChainModule,
     ]
 })
 export class CarModule implements NestModule {
     configure(consumer: MiddlewareConsumer): void {
-
-        if (!EnvConfig.SKIP_MIDDLEWARE) {
-            consumer
-                .apply(JwtauthenticationMiddleware, HlfcredsgeneratorMiddleware)
-                .forRoutes(CarController);
-        }
-    }
 }
